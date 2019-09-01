d2a737a75984aacc63930792230ed3cd14dee206
Fahad Ansar <44476743+fa16np@users.noreply.github.com>
2019-09-01 04:46:04 -0400

    Update -sp.sh

--- -sp.sh
+++ -sp.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-#Hiding input
+#1. Hiding secret input———————————————
 
-c=$'\n'
+c=$'\n' #new line character
 
+#reading two numbers with -sp paramter
 read -sp "Enter 2 Numbers to Sum: $c" num1 num2
+
+#adding both numbers
 sum=$((num1+num2))
+
+#Printing the sum of two numbers
 echo "$sum"
